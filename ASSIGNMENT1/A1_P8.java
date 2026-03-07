05f74d261d2298dc36089a98bbc08f6520f1b364
CoderDebajyoti <debajyoti.475@gmail.com>
2026-03-08 00:35:36 +0530

    Create A1_P8.java

---
+++ ASSIGNMENT1/A1_P8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class A1_P8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter prefix length: ");
+        int n = sc.nextInt();
+
+        int mask = 0xffffffff << (32-n);
+
+        int a=(mask>>>24)&255;
+        int b=(mask>>>16)&255;
+        int c=(mask>>>8)&255;
+        int d=mask&255;
+
+        System.out.println("Subnet Mask: "+a+"."+b+"."+c+"."+d);
+    }
+}
